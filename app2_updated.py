95b7129dc33ec7786b7b97149cf45fc9a1d71f74
Anshuman Sharma <98218480+AnshuMan1612@users.noreply.github.com>
2025-11-25 19:24:28 +0530

    Update app2_updated.py

--- app2_updated.py
+++ app2_updated.py
@@ -27,7 +27,7 @@ from pathlib import Path
 # === USER CREDENTIALS (edit as needed) ===
 USERS = {
     "user1": "pass1",
-    "user2": "pass2"
+    "Ashish Thakran": "Ashish Thakran123"
 }
 
 # === LOGIN LOGIC ===
